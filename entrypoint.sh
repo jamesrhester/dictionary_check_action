e39a827880c061b0073dca0baf341ed431a2f30d
James.Hester <jxh@ansto.gov.au>
2021-03-01 15:35:04 +1100

    Work around zero exit status by checking output length.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,20 @@
 
 # get latest cod-tools
 
-apt-get update
+#apt-get update
 
-apt-get -y install cod-tools
+#apt-get -y install cod-tools
 
 # run the checks
+# any output from cif_ddlm_dic_check flags
+# a problem, the grep for ":" just makes
+# sure that all lines are output.
 
 shopt -s nullglob
 for file in ./*.dic
 do	
-if !(cif_ddlm_dic_check $file )
+	echo $file
+	if  (cif_ddlm_dic_check $file | grep ":") 
 then echo "Failure"; exit 1;
 fi
 done
